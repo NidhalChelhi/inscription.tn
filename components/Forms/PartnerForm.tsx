cccc711e2669a6a46ddf00c154082b3f31248a66
NidhalChelhi <nidhalelchelhi@gmail.com>
2023-09-09 15:53:45 +0100

    Added Partner Form

--- components/Forms/PartnerForm.tsx
+++ components/Forms/PartnerForm.tsx
@@ -5,109 +5,45 @@ import { useTranslation } from "react-i18next";
 const PartnerForm = ({ formik }: any) => {
   const { t } = useTranslation();
 
-  const fatherStatusOptions = [
-    {
-      label: t("Etat du père"),
-      value: "none",
-    },
-    {
-      label: t("Vivant"),
-      value: "Vivant",
-    },
-    {
-      label: t("Decede"),
-      value: "Decede",
-    },
-  ];
-
-  const motherStatusOptions = [
-    {
-      label: t("Etat de la mère"),
-      value: "none",
-    },
-    {
-      label: t("Vivante"),
-      value: "Vivante",
-    },
-    {
-      label: t("Decedee"),
-      value: "Decedee",
-    },
-  ];
   return (
     <div className="flex flex-col gap-4">
       <p className="text-start font-semibold text-lg">
-        {t("Informations parentales")}
+        {t("Information du conjoint")}
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  w-full gap-4">
-        <CustomSelect
-          options={fatherStatusOptions}
-          name="etatPere"
-          value={formik.values.etatPere}
+        <CustomInput
+          type="text"
+          placeholder={t("Nom du conjoint")}
+          name="partnerNom"
+          value={formik.values.partnerNom}
           onChange={formik.handleChange}
         />
         <CustomInput
           type="text"
-          placeholder={t("Nom du père")}
-          name="nomPere"
-          value={formik.values.nomPere}
+          placeholder={t("Prénom du conjoint")}
+          name="partnerPrenom"
+          value={formik.values.partnerPrenom}
           onChange={formik.handleChange}
         />
         <CustomInput
           type="text"
-          placeholder={t("Prénom du père")}
-          name="prenomPere"
-          value={formik.values.prenomPere}
+          placeholder={t("Profession du conjoint")}
+          name="partnerProfession"
+          value={formik.values.partnerProfession}
           onChange={formik.handleChange}
         />
         <CustomInput
           type="text"
-          placeholder={t("Profession du père")}
-          name="professionPere"
-          value={formik.values.professionPere}
+          placeholder={t("Etablissement partner")}
+          name="partnerEtablissement"
+          value={formik.values.partnerEtablissement}
           onChange={formik.handleChange}
         />
         <CustomInput
           type="text"
-          placeholder={t("Etablissement de père")}
-          name="etablissementPere"
-          value={formik.values.etablissementPere}
-          onChange={formik.handleChange}
-        />
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  w-full gap-4">
-        <CustomSelect
-          options={motherStatusOptions}
-          name="etatMere"
-          value={formik.values.etatMere}
-          onChange={formik.handleChange}
-        />
-        <CustomInput
-          type="text"
-          placeholder={t("Nom de la mère")}
-          name="nomMere"
-          value={formik.values.nomMere}
-          onChange={formik.handleChange}
-        />
-        <CustomInput
-          type="text"
-          placeholder={t("Prénom de la mère")}
-          name="prenomMere"
-          value={formik.values.prenomMere}
-          onChange={formik.handleChange}
-        />
-        <CustomInput
-          type="text"
-          placeholder={t("Profession de la mère")}
-          name="professionMere"
-          value={formik.values.professionMere}
-          onChange={formik.handleChange}
-        />
-        <CustomInput
-          type="text"
-          placeholder={t("Etablissement de la mère")}
-          name="etablissementMere"
-          value={formik.values.etablissementMere}
+          placeholder={t("Nombre d'enfants")}
+          name="nbEnfants"
+          value={formik.values.nbEnfants}
           onChange={formik.handleChange}
         />
       </div>
