d89e8f3d7473060c91207505dc2a1c1e187c450f
NidhalChelhi <nidhalelchelhi@gmail.com>
2023-09-10 20:13:12 +0100

    Fixed Email Button Overflow

--- app/page.tsx
+++ app/page.tsx
@@ -77,7 +77,7 @@ export default function Home() {
           </Link>
           <CustomContactButton />
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 w-full">
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 w-full">
           <CustomLegalNoticeButton />
           <CustomFAQButton />
           <CustomGuideButton />
